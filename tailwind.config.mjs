4a77de6e4d8271d955f6ec1e33f42afb32faf124
Yan Monasterios <yankevyn02@gmail.com>
2024-12-23 09:19:26 -0400

    second commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	//se va a utilizar el darkmode 
+	darkMode:'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
